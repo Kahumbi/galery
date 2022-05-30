47ad6226d1af42ced8900c332accc9e47510dcff
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-05-31 02:35:00 +0300

    Add a for loop to display all images through variable block

--- pictures/views.py
+++ pictures/views.py
@@ -5,7 +5,8 @@ from .models import Category, Photo
 
 def gallery(request):
     categories = Category.objects.all()
-    context = {'categories':categories}
+    photos = Photo.objects.all()
+    context = {'categories':categories, 'photos' : photos}
     
     return render(request, 'pictures/gallery.html', context)
 
